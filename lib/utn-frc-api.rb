88d2dd3664ade90f9d5ab25c9f2adecbb8d8b27b
Rodrigo Ovalle <OvalleRo@users.noreply.github.com>
2017-01-31 13:37:54 -0300

    Agregado conexion y autenticacion a sistema academico3

--- lib/utn-frc-api.rb
+++ lib/utn-frc-api.rb
@@ -2,29 +2,55 @@ require "httparty"
 
 class UTN
 	include HTTParty
+	# debug_output $stdout
 
-	attr_reader :legajo, :dominio, :password
+	attr_reader :legajo, :dominio, :password, :post_response, :get_response, :cookie_hash
 
-	base_uri 'http://wwww.frc.utn.edu.ar'
+	base_uri 'http://www.frc.utn.edu.ar'
 
 	DOMINIOS = [["cbasicas"], ["civil"], ["computos"], ["decanato"], ["egresado"], ["electrica"], ["electronica"], ["extension"], ["industrial"], ["mecanica"], ["metalurgica"], ["org"], ["posgrado"], ["punilla"], ["quimica"], ["radio"], ["sa"], ["sae"], ["scdt"], ["sistemas"], ["tecnicatura"], ["virtual"], ["frc"]]
 
 	def initialize(legajo = "", dominio = 0, password = "")
 		@legajo = legajo
-		@dominio = DOMINIOS[dominio]
+		@dominio = DOMINIOS[dominio][0]
 		@password = password
 
-		get_response = self.class.get('/logon.frc')
+		@get_response = self.class.get('/logon.frc')
 
-		#post_response = self.class.post(
-		#		'/logon.frc',
+		@cookie_hash = cookie(@get_response.headers['Set-Cookie'])
+		@cookie_hash = cookie("pag=2; rec=0; usr=#{@legajo}%40#{@dominio}.frc.utn.edu.ar")
 
-		#	)
+
+		@post_response = self.class.post(
+			'/funciones/sesion/iniciarSesion.frc',
+			:body => {
+				page: 'login',
+				pwdClave: @password,
+				redir: '/logon.frc',
+				t: '79845687',
+				txtDominios: @dominio,
+				txtUsuario: @legajo,
+				userid: 'userid'#,btnEnviar: '  Iniciar Sesión  '
+
+				},
+				headers: {'Cookie' => @cookie_hash.to_cookie_string}
+				)
 	end
 
 	def self.indice_dominio
 		DOMINIOS.each.with_index(1) do |valor, i|
-			puts "#{i}: #{valor}"
+			puts "#{i}: #{valor[0]}"
 		end
 	end
+
+	
+	def cookie(cookie_string)
+
+		
+		cookie_hash = CookieHash.new
+		cookie_hash.add_cookies(cookie_string)
+		cookie_hash
+
+	end
+
 end
